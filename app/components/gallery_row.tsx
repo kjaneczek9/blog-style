56f67d09cbcd76ef13e0a3bf95bf9f2daec456ca
kristenjaneczek <57381459+kristenjaneczek@users.noreply.github.com>
2024-02-24 13:43:26 -0800

    gallery!!!

---
+++ app/components/gallery_row.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import GalleryImage from './gallery_index';
+
+interface ComponentProps {
+  images: { thumbnailPath: string; link: string; }[];
+}
+
+const Component: React.FC<ComponentProps> = ({ images }) => {
+  return (
+    <section>
+      <div className='flex justify-center'>
+        {images.map((image, index) => (
+          <GalleryImage key={index} thumbnailPath={image.thumbnailPath} link={image.link} />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default Component;

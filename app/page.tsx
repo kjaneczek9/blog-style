8c9f7441b28370a0cb8ff24dd47b9b9a81496902
kristenjaneczek <57381459+kristenjaneczek@users.noreply.github.com>
2024-02-24 00:22:34 -0800

    Remove unneeded form

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { MdPlace } from 'react-icons/md';
+
 export default function Page() {
   return (
 <div className="page-wrapper">
@@ -7,7 +9,9 @@ export default function Page() {
         <div className="w-80 h-80 custom-color rounded-full opacity-20 absolute top-1/2 transform -translate-y-1/2 -translate-x-20"/>
         <h1 className="font-medium text-5xl mb-4 tracking-tighter pl-12">Celeste Bernstein</h1>
         <p className="pl-12">Actor. Writer. Singer. Producer.</p>
+
       </div>
+      
 
       <div>
         <img src="/Headshot.jpg" alt="Description of the image" className="ml-10 mt-14 border-4 border-gray-300 rounded-lg" style={{ width: '500px', height: 'auto' }} />

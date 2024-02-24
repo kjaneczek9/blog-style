b6c8e084bc0acb798fe8d7c1881a7109a52592a1
kjaneczek9 <36136051+kjaneczek9@users.noreply.github.com>
2024-02-24 00:29:00 -0800

    Remove unneeded form (#2)
    * Remove unneeded form
    
    * Remove unneeded gallery
    
    * Remove unneeded md
    
    ---------
    
    Co-authored-by: kristenjaneczek <57381459+kristenjaneczek@users.noreply.github.com>

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -20,13 +20,13 @@ const navItems = {
 
 export function Navbar() {
   return (
-    <div className="position-sticky top-0 flex fixed z-50 w-full h-[70px] no-underline bg-white">
+    <div className="position-sticky top-0 flex fixed z-50 w-full h-[70px] no-underline nav-bar-color">
       {Object.entries(navItems).map(([path, { name }]) => {
         return (
           <Link
             key={path}
             href={path}
-            className="transition-all items-center justify-center hover:text-neutral-800 dark:hover:text-neutral-200 m-auto text-lg font-bold"
+            className="transition-all items-center justify-center hover:text-neutral-800 dark:hover:text-neutral-200 m-auto text-lg font-italic"
           >
             {name}
           </Link>

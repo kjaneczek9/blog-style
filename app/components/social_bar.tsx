a6d9eaaf00ca53b8e137687eb3708cfc251097b5
kristenjaneczek <57381459+kristenjaneczek@users.noreply.github.com>
2024-02-28 21:23:49 -0800

    Contact page goes to email. verification evidence is in my sent inbox.

--- app/components/social_bar.tsx
+++ app/components/social_bar.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Icon from './social_icon'; // assuming Icon component is in the same directory
 
+const clientEmail = "gotoceleste@gmail.com";
+const subject = "Ur mom";
+const body = "Ur mom gae";
+
+const mailtoLink = `mailto:${clientEmail}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+
 const Component: React.FC = () => {
   return (
     <div style={{ width: '300px' }}>
       {/* Container for icons with flexbox */}
       <div className="flex items-center">
-      <Icon thumbnailPath='/linkedin.png' link='https://www.linkedin.com'/>
-      <Icon thumbnailPath='/email.png' link='https://www.google.com' />
+      <Icon thumbnailPath='/linkedin.png' link='https://www.linkedin.com/in/celeste-bernstein-1756a71b0/'/>
+      <Icon thumbnailPath='/email.png' link={mailtoLink} />
       <Icon thumbnailPath='/linkedin.png' link='https://www.linkedin.com' />
       </div>
     </div>

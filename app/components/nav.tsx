126df46e423d8e45399f6a9ba45869d01f0f0b35
Kristen <36136051+kjaneczek9@users.noreply.github.com>
2024-02-28 21:24:49 -0800

    Contact page goes to email. verification evidence is in my sent inbox. (#9)
    Co-authored-by: kristenjaneczek <57381459+kristenjaneczek@users.noreply.github.com>

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,5 +1,9 @@
 import Link from 'next/link';
+const clientEmail = "gotoceleste@gmail.com";
+const subject = "Ur mom";
+const body = "Ur mom gae";
 
+const mailtoLink = `mailto:${clientEmail}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
 const navItems = {
   '/': {
     name: 'Home',
@@ -13,7 +17,7 @@ const navItems = {
   '/gallery': {
     name: 'Gallery',
   },
-  '/contact': {
+  [mailtoLink]: {
     name: 'Contact',
   },
 };
@@ -22,15 +26,28 @@ export function Navbar() {
   return (
     <div className="position-sticky top-0 flex fixed z-50 w-full h-[70px] no-underline nav-bar-color">
       {Object.entries(navItems).map(([path, { name }]) => {
-        return (
-          <Link
-            key={path}
-            href={path}
-            className="transition-all items-center justify-center hover:text-neutral-800 dark:hover:text-neutral-200 m-auto text-lg font-italic"
-          >
-            {name}
-          </Link>
-        );
+        if (path == mailtoLink) {
+          return (
+            <Link
+              key={path}
+              href={path}
+              target="_blank"
+              className="transition-all items-center justify-center hover:text-neutral-800 dark:hover:text-neutral-200 m-auto text-lg font-italic"
+            >
+              {name}
+            </Link>
+          );
+        } else {
+          return (
+            <Link
+              key={path}
+              href={path}
+              className="transition-all items-center justify-center hover:text-neutral-800 dark:hover:text-neutral-200 m-auto text-lg font-italic"
+            >
+              {name}
+            </Link>
+          );
+        }
       })}
   </div>
   );
